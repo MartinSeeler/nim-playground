da931f1a2932eba8c46580a41d59e7618dd89068
Martin Seeler <mseeler@avantgarde-labs.de>
2015-02-03 00:29:50 +0100

    Solve problem 18

--- euler_018.nim
+++ euler_018.nim
@@ -1,16 +1,21 @@
 import strutils
 
-type
-  Tree = ref object
-    value: int
-    left: ref Tree
-    right: ref Tree
-
 const input = """
-3
-7 4
-2 4 6
-8 5 9 3
+75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
 """
 
 const lines = splitLines(input)
@@ -20,16 +25,19 @@ proc parseLine(line: string): seq[int] =
   for number in split(line):
     result.add(parseInt($number))
 
-proc parseTree(lines: seq[string], t: var ref Tree): Tree = 
+proc parseTree(lines: seq[string], sum: int = 0, index: int = 0): int = 
   if lines.len == 0:
-    discard
+    # echo "Reached sum $# and parent $#" % [$sum, $parent]
+    sum
   else:
     let nums = parseLine(lines[0])
+    let subLines = lines[1..lines.len-1]
+    var max = 0
     for i, x in nums:
-      t.value = x
-      echo x, i
-    echo nums
-    discard parseTree(lines[1..lines.len-1], t)
+      if i in {index, index + 1}:
+        # echo "will call with sum = $# and index = $#" % [$(sum + x), $i]
+        let newSum = parseTree(subLines, sum + x, i)
+        if newSum > max: max = newSum
+    max
 
-var root = new(Tree)
-discard parseTree(lines[0..lines.len-2], root)
+echo parseTree(lines[0..lines.len-2])
